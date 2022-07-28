efbceaa44d292737ba22a3a0023d3497c49151f2
sasse7 <106542497+sasse7@users.noreply.github.com>
2022-07-28 11:02:59 -0400

    Add files via upload

---
+++ BookstoreSorting/src/main/java/com/springboot/bookcart/model/Cart.java
@@ -0,0 +1,40 @@
+package com.springboot.bookcart.model;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+
+@Entity
+@Table(name="Cart")
+@Data public class Cart {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+ private long id;
+	
+	@Column(name = "username", nullable = false)
+	 private String username;
+	
+	@Column(name = "bookid1")
+	private int bookid1;
+	
+	@Column(name = "bookid2")
+	private int bookid2;
+	
+	@Column(name = "bookid3")
+	private int bookid3;
+	
+	@Column(name = "bookid4")
+	private int bookid4;
+
+	@Column(name = "bookid5")
+	private int bookid5;
+	
+	@Column(name = "bookid6")
+	private int bookid6;
+}
